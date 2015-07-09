024f3105dff000906e789e6f58ead74a3985e9a3
Qian Guo <qiguo@redhat.com>
2015-07-09 16:40:11 +0800

    fix :

--- netperf.sh
+++ netperf.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 host_ip=$1
-times=60
+times=3
 protos="TCP_RR TCP_CRR UDP_RR TCP_STREAM TCP_MAERTS TCP_SENDFILE UDP_STREAM"
+
 if [ $# -eq 0 ]
 then
     echo "please provide the netserver_ip. e.g. `basename $0` 10.66.9.130 "
     exit 1
 fi
-for proto in $protos:
+for proto in $protos
 do
+    echo "test $proto"
     netperf -H $host_ip -l $times -t $proto
     sleep 3
 done
