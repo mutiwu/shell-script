fd9ace73b340729a65047cc0824c465cea57fa00
Qian Guo <qiguo@redhat.com>
2015-07-09 16:20:40 +0800

    fix sytax error

--- netperf.sh
+++ netperf.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-host_ip = $1
-times = 60
+host_ip=$1
+times=60
 protos="TCP_RR TCP_CRR UDP_RR TCP_STREAM TCP_MAERTS TCP_SENDFILE UDP_STREAM"
 if [ $# -eq 0 ]
 then
     echo "please provide the netserver_ip. e.g. `basename $0` 10.66.9.130 "
     exit 1
+fi
 for proto in $protos:
 do
     netperf -H $host_ip -l $times -t $proto
