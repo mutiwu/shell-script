fd5f8a595794b2ed509647a12dc440421fd6f07b
guoqian <guoqian@nco-china.com>
2016-04-25 18:09:08 +0800

    update script

--- fio_test/per_fio.sh
+++ fio_test/per_fio.sh
@@ -2,10 +2,10 @@
 ndate=$(echo `date`|sed 's/[[:space:]]/\_/g')
 tdir=/tmp/fio_$ndate
 mkdir -p $tdir
+printf "RW\tbs\tiodepth\tBW\t\tIOPS\t\t\n">>$tdir/FINAL_DATA
 for f in seq_r seq_w rand_r rand_w
 do
 rdir=$tdir/$f/crs
-
     for b in 4k 16k 64k 256k
     do
         for d in 1 8 64
@@ -17,6 +17,9 @@ rdir=$tdir/$f/crs
                 fio $f\.ini --bs=$b --iodepth=$d --output=$crsdir/fio_rs_$f$b$d$i
             done
 	    grep -h '^[[:space:]].*io\=.*bw\=.*iops\=' $crsdir/fio_rs_$f$b$d* >>$tdir/$f/merg_$f$b$d
+	    bw_rs=`cut -d ':' $tdir/$f/merg_$f$b$d -f 2 | cut -d ',' -f 2 |cut -d '=' -f 2 |awk -F K '{print $1}' |awk '{sum += $1};END {print sum/5}'`
+	    iops_rs=`cut -d ':' $tdir/$f/merg_$f$b$d -f 2 | cut -d ',' -f 3 |cut -d '=' -f 2 |awk -F K '{print $1}' |awk '{sum += $1};END {print sum/5}'`
+	    printf "$f\t$b\t$d\t$bw_rs\t\t$iops_rs\t\t\n">>$tdir/FINAL_DATA
         done
     done
 done
